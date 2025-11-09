cb261bcc19b7b2487b27d3c3b70af46afe2bd2f7
Kousalya98 <kousalyapanchangam98@gmail.com>
2025-11-09 19:12:05 +0530

    feat:initical spring boot setup done

---
+++ backend/BackendApplication/src/main/java/com/Backend/BackendApplication/BackendApplication.java
@@ -0,0 +1,14 @@
+package com.Backend.BackendApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendApplication.class, args);
+		System.out.println("Backend Application is running...");
+	}
+
+}
